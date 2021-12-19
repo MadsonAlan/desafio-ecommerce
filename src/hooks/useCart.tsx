0e866204dca6784aa5abf0360e61b9dff69da045
MadsonAlan <madson99hitech@gmail.com>
2021-12-19 16:16:02 -0300

    Resolução do desafio

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -35,17 +35,56 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
       // TODO
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+      const updateCart = [...cart]
+      const productExists = updateCart.find(product => product.id === productId)
+
+      const stock = await api.get(`stock/${productId}`)
+
+      const stockAmount = stock.data.amount
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      if (productExists) {
+        productExists.amount = amount
+
+      } else {
+        const product = await api.get(`products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+        updateCart.push(newProduct)
+      }
+      setCart(updateCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart))
     } catch {
       // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
       // TODO
+      const updateCart = [...cart]
+      const productIndex = updateCart.findIndex(product => product.id === productId)
+
+      if (productIndex !== -1) {
+        updateCart.splice(productIndex, 1);
+        setCart(updateCart)
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart))
+      } else {
+        throw Error()
+      }
     } catch {
       // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -55,8 +94,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   }: UpdateProductAmount) => {
     try {
       // TODO
+      if (amount <= 0) {
+        return;
+      }
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updateCart = [...cart]
+
+      const productExists = updateCart.find(product => product.id === productId)
+
+      if (productExists) {
+        productExists.amount = amount
+        setCart(updateCart)
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart))
+
+      } else {
+        throw Error()
+      }
     } catch {
       // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,17 +27,23 @@ const Home = (): JSX.Element => {
 
   const cartItemsAmount = cart.reduce((sumAmount, product) => {
     // TODO
-    return {
-      key: product.id,
-      amount: sumAmount
-    }
+    const newSumAmount = { ...sumAmount }
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount
+
   }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
       try {
-        const response = await api.get('products');
-        setProducts(response.data)
+        const response = await api.get<Product[]>('products');
+        const data = response.data.map(product => ({
+          ...product,
+          priceFormatted: formatPrice(product.price)
+        }))
+
+        setProducts(data)
 
       } catch (error) {
 
@@ -59,10 +65,7 @@ const Home = (): JSX.Element => {
           <li key={product.id}>
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
-            <span>{new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL'
-            }).format(product.price)}</span>
+            <span>{product.priceFormatted}</span>
             <button
               type="button"
               data-testid="add-product-button"
